4653da7f22f1a4bd9f34644b24173533bed4d005
Adil Rahimov <133156253+adilmrcodeaz@users.noreply.github.com>
2025-01-26 00:27:08 +0400

    Create main.py

---
+++ data/main.py
@@ -0,0 +1,20 @@
+from llama_index.llms.ollama import Ollama
+from llama_parse import LlamaParse
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
+from llama_index.core.embeddings import resolve_embed_model
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.agent import ReActAgent
+from pydantic import BaseModel
+from llama_index.core.output_parsers import PydanticOutputParser
+from llama_index.core.query_pipeline import QueryPipeline
+from prompts import context, code_parser_template
+from code_reader import code_reader
+from dotenv import load_dotenv
+import os
+import ast
+
+load_dotenv()
+
+llm = Ollama(model="mistral", request_timeout=30.0)
+
+parser = LlamaParse(result_type="markdown")
